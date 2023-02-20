9b09e12b6873c54bb75866ee8a512cf95146bc11
Juliana Virgens <julianalv1@hotmail.com>
2023-02-20 00:58:59 -0300

    Update jogo_forca.py
    Deixando o código mais limpo com funções.

--- 01-Jogo_forca/jogo_forca.py
+++ 01-Jogo_forca/jogo_forca.py
@@ -1,10 +1,11 @@
 import random # Agora que já temos todas as palavras na lista devemos acessá-las aleatoriamente. Para isso, vamos importar a biblioteca random.
 
-def jogar():
+def imprime_mensagem_abertura():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+def carrega_palavra_secreta():
     arquivo = open("palaras.txt", "r") # De início devemos abrir o arquivo, e como já sabemos é uma boa prática fechá-lo ao final(arquivo.close()).
     palavras = []
 
@@ -17,8 +18,19 @@ def jogar():
     numero = random.randrange(0, len(palavras)) # Com a biblioteca já disponível temos que acessar uma das palavras incluídas na nossa lista. Para isso será necessário gerar um número com a posição aleatória. O número gerado deveria ser apenas de índices válidos na lista: 0 até o tamanho da lista.
 
     palavra_secreta = palavras[numero].upper() # Com o número gerado basta agora pegarmos a palavra secreta correspondente a essa posição.
+    return palavra_secreta #como não existe fora do código usamos o return.
 
-    letras_acertadas = ["_" for letra in palavra_secreta] # vai criar um traço para cada letra
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]  
+
+
+def jogar():
+
+    imprime_mensagem_abertura
+
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta) #["_" for letra in palavra_secreta] # vai criar um traço para cada letra
 
     enforcou = False
     acertou = False
