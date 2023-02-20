b41a91b6aac9fe94fed01be22192763d943c4d53
Juliana Virgens <julianalv1@hotmail.com>
2023-02-19 23:50:35 -0300

    Update arquivo-txt.py

--- 01-Jogo_forca/arquivo-txt.py
+++ 01-Jogo_forca/arquivo-txt.py
@@ -2,6 +2,11 @@
 
 # No prompt de comando, criar o arquivo:
 # open("nomedoarquivo.txt","w") {w-write; r-read; a-append (acrescentar)} sempre nessa ordem, o nome do arquivo e o modificador de acesso.
+# Importante é que não precisamos passar o modificador de acesso, pois o segundo parâmetro é opcional:
+# arquivo = open("entrada.txt")
+#Nesse caso será utilizado o modo de leitura (r) por padrão.
+
+
 # abrimos um arquivo de texto
 # escreve o que quer no arquivo: arquivo.write("banana\n") fazer dessa forma vai criar uma lista, com uma palavra abaixo da outra.
 # arquivo.write("maça\n")...
