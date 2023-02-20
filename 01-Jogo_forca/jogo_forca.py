a0b4a0b6aa9186631ed3b2f6d545a070b31050ca
Juliana Virgens <julianalv1@hotmail.com>
2023-02-20 00:31:29 -0300

    Update jogo_forca.py
    Jogo com novas funções

--- 01-Jogo_forca/jogo_forca.py
+++ 01-Jogo_forca/jogo_forca.py
@@ -1,10 +1,24 @@
+import random # Agora que já temos todas as palavras na lista devemos acessá-las aleatoriamente. Para isso, vamos importar a biblioteca random.
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    arquivo = open("palaras.txt", "r") # De início devemos abrir o arquivo, e como já sabemos é uma boa prática fechá-lo ao final(arquivo.close()).
+    palavras = []
+
+    for linha in arquivo: # Depois temos que criar uma lista e percorrer o arquivo. Cada linha do arquivo deve ser guardada nessa lista.
+        linha = linha.strip() # Precisamos remover o \n ao final da linha, fazendo um strip nela.
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras)) # Com a biblioteca já disponível temos que acessar uma das palavras incluídas na nossa lista. Para isso será necessário gerar um número com a posição aleatória. O número gerado deveria ser apenas de índices válidos na lista: 0 até o tamanho da lista.
+
+    palavra_secreta = palavras[numero].upper() # Com o número gerado basta agora pegarmos a palavra secreta correspondente a essa posição.
+
+    letras_acertadas = ["_" for letra in palavra_secreta] # vai criar um traço para cada letra
 
     enforcou = False
     acertou = False
