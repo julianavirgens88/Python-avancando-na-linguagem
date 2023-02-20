037621b4f40714ee6a9f3d2c5ca97ef3ff6be0ee
Juliana Virgens <julianalv1@hotmail.com>
2023-02-19 23:46:42 -0300

    Create arquivo-txt.py
    Explicação sobre arquivo de texto.

---
+++ 01-Jogo_forca/arquivo-txt.py
@@ -0,0 +1,11 @@
+# Para criar um arquivo que pegue as palavras e a máquina escolha qual vai utilizar para o jogador:
+
+# No prompt de comando, criar o arquivo:
+# open("nomedoarquivo.txt","w") {w-write; r-read; a-append (acrescentar)}
+# abrimos um arquivo de texto
+# escreve o que quwr no arquivo: arquivo.write("banana\n") fazer dessa forma vai criar uma lista, com uma palavra abaixo da outra.
+# arquivo.write("maça\n")...
+# arquivo.close() - para fechar o arquivo
+# no gitbash encontra o arquivo pelo comando ls
+# para abrir esse arquivo: cat nomedoarquivo.txt - vai aparecer todas as palavras que foram colocadas. Não usamos o cd como de costume.
+# mv palaras.txt PycharmProjects/pythonProject - movemos o arquivo para para o Pycharm para seguir usando o código.
